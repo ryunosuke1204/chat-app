a2fa7ebb086a6bba13ee3e3fc1ee36dfd35e3993
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-05-04 14:40:40 +0900

    チャットルームの削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,26 @@
 class MessagesController < ApplicationController
+
   def index
-   @massage = Massage.new
-   @room = Room.find(paramas[:room_id]) 
-   @message = @room.messages.includes(:user)
-  end
-
-  def create
-    @room= Room.find(:room_id)
-    @massage = @room.messages.new(message_params)
-    if @message.save
- redirect_to_room_massage_path(@room)
- else
-  @message = @room.messages.includes(:user)
-  render :index
+     @message = Message.new
+     @room = Room.find(params[:room_id])
+     @messages = @room.messages.includes(:user)
+   end
+ 
+   def create
+     @room = Room.find(params[:room_id])
+     @message = @room.messages.new(message_params)
+     if @message.save
+       redirect_to room_messages_path(@room)
+     else
+       @messages = @room.messages.includes(:user)
+       render :index
+     end
+   end
+ 
+   private
+ 
+   def message_params
+     params.require(:message).permit(:content).merge(user_id: current_user.id)
+   end
  end
-    end
-
- private
-
- def message_params
-  params.require(:message).permit.maege(user_id: current_user.id)
- end
-end
-
+ 
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2022_05_04_003442) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
