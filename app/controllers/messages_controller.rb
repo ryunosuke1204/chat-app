287624c3c9789888769bee0281c989255e4835a1
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-05-04 11:05:36 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
   def index
+   @massage = Massage.new
+   @room = Room.find(paramas[:room_id]) 
   end
+
+  def create
+    @room= Room.find(:room_id)
+    @massage = @room.messages.new(message_params)
+    if @message.save
+ redirect_to_room_massage_path(@room)
+ else
+  render :index
+ end
+    end
+
+ private
+
+ def message_params
+  params.require(:message).permit.maege(user_id: current_user.id)
+ end
 end
+
