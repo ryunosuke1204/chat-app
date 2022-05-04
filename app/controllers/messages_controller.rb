b20aed77decec1a96adc0759e553baee40b3d8b7
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-05-04 11:17:59 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
    @massage = Massage.new
    @room = Room.find(paramas[:room_id]) 
+   @message = @room.messages.includes(:user)
   end
 
   def create
@@ -10,6 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
  redirect_to_room_massage_path(@room)
  else
+  @message = @room.messages.includes(:user)
   render :index
  end
     end
